51795a3084e1db1bec2dd59316cf7db66ff5b7d5
TaeWooKim-SCH <zop1234@hanmail.net>
2025-02-16 15:05:38 +0900

    feat: payment 모델 정의

--- MC-server.Core/Models/User.cs
+++ MC-server.Core/Models/User.cs
@@ -35,5 +35,7 @@ namespace MC_server.Core.Models
         [Column("created_at", TypeName = "datetime2")] // 데이터 타입 명시
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)] // 데이터베이스에서 기본값 설정
         public DateTime CreatedAt { get; set; }
+
+        public virtual ICollection<Payment>? Payments { get; set; } // User와 Payment 관계 추가
     }
 }
