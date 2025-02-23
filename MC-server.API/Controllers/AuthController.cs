4d81d7a66ba4729bf422cbe061f38103d575bc69
TaeWooKim-SCH <zop1234@hanmail.net>
2025-02-23 14:46:19 +0900

    test: 테스트 로그 추가

--- MC-server.API/Controllers/AuthController.cs
+++ MC-server.API/Controllers/AuthController.cs
@@ -28,7 +28,7 @@ namespace MC_server.API.Controllers
 
             // 1. Google Auth Code 검증 (토큰 교환)
             var tokenResponse = await _googleAuthService.ExchangeAuthCodeForTokenAsync(request.UserId, request.AuthCode);
-
+            Console.WriteLine(tokenResponse.IdToken);
             // 2. 토큰을 이용해 사용자 정보 가져오기
             var user = _googleAuthService.GetUserInfo(tokenResponse.IdToken);
             Console.WriteLine(user.Name);
