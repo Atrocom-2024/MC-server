d6f9188e7f1111fce0e19036adec3bfa29aeaeb6
TaeWooKim-SCH <zop1234@hanmail.net>
2025-02-24 15:58:16 +0900

    feat: 게스트 인증 구현

--- MC-server.API/Controllers/AuthController.cs
+++ MC-server.API/Controllers/AuthController.cs
@@ -41,6 +41,21 @@ namespace MC_server.API.Controllers
             // 3. 사용자 정보 가져오기 -> 유저 정보가 없다면 자동 회원가입
             var userInfo = await _userApiService.GetUserDetailsForApiAsync(request.UserId)
                 ?? await _userApiService.CreateUserAsync(request.UserId, "google");
+
+            return Ok(userInfo);
+        }
+
+        [HttpPost("guest")]
+        public async Task<IActionResult> GuestAuth([FromBody] GuestAuthRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.UserId))
+            {
+                throw new ValidationException("UserId is required.");
+            }
+            // 사용자 정보 가져오기 -> 유저 정보가 없다면 자동 회원가입
+            var userInfo = await _userApiService.GetUserDetailsForApiAsync(request.UserId)
+                ?? await _userApiService.CreateUserAsync(request.UserId, "guest");
+
             return Ok(userInfo);
         }
     }
